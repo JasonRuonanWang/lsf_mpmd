84b547807f76d947eeb297d32aa6f8b299c420fd
Ruonan Wang <jason.ruonan.wang@gmail.com>
2020-02-03 23:48:15 -0500

    added file writing

--- lsf_mpmd.py
+++ lsf_mpmd.py
@@ -7,13 +7,16 @@ rankCurrent = 0
 appCurrent = 0
 cpuCurrent = 0
 
+file1 = open("job.erf","w")
+
+
 for app in ranks:
     for rank in range(app):
         if rankCurrent % ranksPerNode == 0:
             cpuCurrent = 0
         host = int(rankCurrent / ranksPerNode)
         line = "rank: {0}: {{ host: {1}; cpu: {{{2}-{3}}} }} : app {4}".format(rankCurrent, host, cpuCurrent, cpuCurrent + 3, appCurrent)
-        print(line)
+        file1.write(line + "\n")
         cpuCurrent = cpuCurrent + cpusPerRank
         rankCurrent = rankCurrent + 1
     appCurrent = appCurrent + 1
